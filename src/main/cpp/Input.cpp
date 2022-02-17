de93c6407ef133db5401ee8166ce430e0cb54cfb
unknown <attempted06@gmail.com>
2022-02-16 19:02:59 -0500

    Numerically ordered the controller buttons

--- src/main/cpp/Input.cpp
+++ src/main/cpp/Input.cpp
@@ -13,15 +13,16 @@ Input::Input() {
   joystick = new frc::Joystick(0);
   
   /** Assigning all the buttons their values */
-  xBtn = new frc2::JoystickButton(joystick, 2);
   squareBtn = new frc2::JoystickButton(joystick, 1);
-  triangleBtn = new frc2::JoystickButton(joystick, 4);
+  xBtn = new frc2::JoystickButton(joystick, 2);
   circleBtn = new frc2::JoystickButton(joystick, 3);
-  l2Btn = new frc2::JoystickButton(joystick, 7);
+  triangleBtn = new frc2::JoystickButton(joystick, 4);
   l1Btn = new frc2::JoystickButton(joystick, 5);
-  r2Btn = new frc2::JoystickButton(joystick, 8);
   r1Btn = new frc2::JoystickButton(joystick, 6);
+  l2Btn = new frc2::JoystickButton(joystick, 7);
+  r2Btn = new frc2::JoystickButton(joystick, 8);
   shareBtn = new frc2::JoystickButton(joystick, 9);
   optionsBtn = new frc2::JoystickButton(joystick, 10);
   rightStickBtn = new frc2::JoystickButton(joystick, 12);
+
 }
